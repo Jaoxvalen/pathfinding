af4dda611bd40961b7a5c17789b04930cd401ea3
Richard Yantas Alcantara <richardyantas5@gmail.com>
2017-10-12 18:33:16 -0500

    Place.h and InputFile.h was added

---
+++ InputFile.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "CGraph.h"
+#include <string>
+#include <fstream>
+
+using namespace DS;
+
+class InputFile{
+    public:
+    void readInput(string path,CGraph<Place,float> graph){
+        ifstream in;
+        in.open(path);
+        string s,line;
+        double n;
+        while(getline(in,line)){
+            
+        }
+    }
+}
---
+++ Place.h
@@ -0,0 +1,9 @@
+#pragma once
+class Place{
+    public:
+    double x,y;
+    Place(double _x,double _y){
+        x = _x;
+        y = _y;
+    }    
+}
