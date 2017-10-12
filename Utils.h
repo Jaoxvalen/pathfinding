63ea883dd4ddeb2314f7658ca6cc0b256b93cbc6
Jaoxvalen <jaoxvalen@gmail.com>
2017-10-12 17:54:01 -0500

    added getword()

--- Utils.h
+++ Utils.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <math.h>
 #include <time.h>
+#include <fstream>
+
+using namespace std;
 class Utils
 {
 public:
@@ -15,5 +18,23 @@ static int initialize_randomness(int seed)
             seed1 = seed;
         srand(seed1);
         return seed1;
+    }
+    
+    static void getWord(ifstream& fichero, string& line)
+    {
+        line = "";
+        char c = ' ';
+        bool scaning = false;
+        while((c == ' ' || c == '\n' || c == '\r') || scaning) {
+            fichero.get(c);
+            if(c != ' ' && c != '\n' && c != '\r') {
+                scaning = true;
+                line += c;
+            } else {
+                if(scaning) {
+                    break;
+                }
+            }
+        }
     }
 };
